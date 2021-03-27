9b70bafc8954e6f3348e99865f0aece57974b809
Bodrogi_Lili <bodrogilili@stud.uni-obuda.hu>
2021-03-27 22:17:55 +0100

    Created the Repositories.

---
+++ Haunted.GameModel/IGameModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haunted.GameModel
+{
+    interface IGameModel
+    {
+        public bool[,] Walls { get; set; }
+        public Point Player { get; set; }
+        public Point Exit { get; set; }
+        public Point Key { get; set; }
+        public double GameWidth { get;  set; } // Pixel size
+        public double GameHeight { get;  set; } // Pixel size
+        public double TileSize { get; set; }
+    }
+}
---
+++ Haunted.Repository/IStorageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Haunted.Repository
+{
+    interface IStorageRepository
+    {
+        public void GetData(XDocument data);
+    }
+}
