3741d5a01ae8cc8810adf651c174628102f12e7b
Bodrogi_Lili <bodrogilili@stud.uni-obuda.hu>
2021-04-15 12:52:20 +0200

    Added some classes.

---
+++ Haunted/Haunted.GameModel/Element.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Haunted.GameModel
+{
+    class Element
+    {
+        protected Rectangle area;
+
+        public Rectangle Area { get { return this.area; } }
+
+        public Point GetPoints { get { return new Point(this.area.X, this.area.Y); } }
+        public void ChangeX(int x)
+        {
+            this.area.X += x;
+        }
+        public void ChangeY(int y)
+        {
+            this.area.Y += y;
+        }
+        public void SetPosition(int x, int y)
+        {
+            this.area.X = x;
+            this.area.Y = y;
+        }
+    }
+}
