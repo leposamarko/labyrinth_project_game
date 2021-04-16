79b9387868fe699c78357384cf24f4e680300ba2
Bodrogi_Lili <bodrogilili@stud.uni-obuda.hu>
2021-04-16 14:41:36 +0200

    Added a config file but the values are missing.

---
+++ Haunted/Haunted.GameModel/Config.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haunted.GameModel
+{
+    class Config //ezeknek még értéket kell adni
+    {
+        //sziv
+        public static double HeartPlaceX;
+        public static double HeartPlaceY;
+        public static double HeartSize;
+        //stopper
+        public static double TimePlaceX;
+        public static double TimePlaceY;
+        public static double TimeTextSize;
+        //szellem
+        public static double GhostHeight;
+        public static double GhostWidht;
+        public static double GhostSpeed; //(egyenlőre csak 1 sebesség))
+        //player
+        public static double PlayerWidht;
+        public static double PlayerHeight;
+        //játék
+        public static double GameWidht;
+        public static double GameHeight;
+
+    }
+}
