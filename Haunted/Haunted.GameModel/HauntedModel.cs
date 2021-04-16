228b46abdec889d95ef6d0ddbdbbd62d4ca4b84b
Bodrogi_Lili <bodrogilili@stud.uni-obuda.hu>
2021-04-16 14:42:47 +0200

    Doing the Model classes.

--- Haunted/Haunted.GameModel/HauntedModel.cs
+++ Haunted/Haunted.GameModel/HauntedModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Windows;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +16,28 @@ namespace Haunted.GameModel
         public double GameWidth { get; set; }
         public double GameHeight { get; set; }
         public double TileSize { get; set; }
+
+
+        const int numGhost = 5;
+        private int lifeCount = 3;
+        private int numKey = 4;
+
+        public HauntedModel()
+        {
+
+        }
+        public static int NumGhost { get { return numGhost; } }
+
+        public int LivesCount { get => this.lifeCount; set => this.lifeCount = value; }
+
+        public Time myTime { get; set; }
+
+        int IGameModel.numGhost { get { return 5; } }
+
+        int IGameModel.numKey { get { return 4; } }
+
+        public List<Ghost> Ghosts { get; set; }
+        public List<Key> Keys { get; set; }
+        public Point[] Background { get; set; }
     }
 }
--- Haunted/Haunted.GameModel/IGameModel.cs
+++ Haunted/Haunted.GameModel/IGameModel.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Windows;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Haunted.GameModel
 {
-    public interface IGameModel
+    public interface IGameModel //megj.: Az órához stopwatch-ot kell használni, melyet xy.stop() után .elapsed-el el lehet menteni egy timespanbe -> https://stackoverflow.com/questions/13690708/timer-c-sharp-use-in-game-development
     {
         public bool[,] Walls { get; set; }
         public Point Player { get; set; }
@@ -16,6 +16,12 @@ namespace Haunted.GameModel
         public double GameWidth { get;  set; } // Pixel size
         public double GameHeight { get;  set; } // Pixel sizes 
         public double TileSize { get; set; }
+        public int LivesCount { get; set; } //nemtom ezek kellenek max majd kiszedjük
+        public int numGhost { get; }
+        public int numKey { get; }
+        List<Ghost> Ghosts { get; set; }
+        List<Key> Keys { get; set; }
+        Point[] Background { get; set; }
     }
 }
  
